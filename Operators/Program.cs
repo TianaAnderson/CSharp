a40df47d782ab56d51c0369e0e69f538da4d1aeb
TianaAnderson <tianamarcelaa@gmail.com>
2019-06-12 16:36:26 -0700

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+
+            var b = 2;
+
+            var c = 3;
+
+            Console.WriteLine( c > b || c == a);
+
+            /*
+            Console.WriteLine(a + b * c);// 1 + (2 * 3) = 7
+            Console.WriteLine((a + b) * c);// (1 + 2) *3 = 9
+            Console.WriteLine((float)a / (float)b);
+            */
+        }
+    }
+}
+ 
