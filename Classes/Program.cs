cbdfee91929ec1cf0310e607de9b7a1418d63ec9
TianaAnderson <tianamarcelaa@gmail.com>
2019-06-12 16:58:06 -0700

    Section 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+
+        }
+
+        public  static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var person = new Person();
+            //var p = person.Parse("Tiana")
+            //person.Name = "Tiana";
+            var person = Person.Parse("Tiana");
+            person.Introduce("Mosh");
         }
     }
 }
---
+++ Properties/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(1990, 10, 24));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
