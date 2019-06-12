144d51b67acd6d5655adbc04c5dd35ae8b03b0e1
TianaAnderson <tianamarcelaa@gmail.com>
2019-06-12 16:37:17 -0700

    Add files via upload

---
+++ TypeConversion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //var number = "1234";
+                ////int i = Convert.ToInt32(number);
+                ////Console.WriteLine(number);
+
+                //byte b = Convert.ToByte(number);
+                //Console.WriteLine(number);
+
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+            //byte b = 1;
+            //int i = b;
+            //Console.WriteLine(i);
+
+
+            //Casting as byte
+            //int i = 1;
+            //byte b = (byte) i;
+            //Console.WriteLine(b);
+
+            
+
+
+        }
+    }
+}
